7af47a508e05df88ea63cc557438b54f055776a6
Robin Appelman <robin@icewind.nl>
2021-11-09 16:35:39 +0100

    migration ci

---
+++ ci-config.php
@@ -0,0 +1,17 @@
+<?php $CONFIG = [
+	'objectstore_multibucket' => [
+		'class' => 'OC\Files\ObjectStore\S3',
+		'arguments' => [
+			'num_buckets' => 64,
+			'bucket' => 'nextcloud-',
+			'autocreate' => true,
+			'key'    => 'dummy',
+			'secret' => 'dummy',
+			'hostname' => 'localhost',
+			'port' => 4566,
+			'use_ssl' => false,
+			'use_path_style' => true,
+			'uploadPartSize' => 52428800,
+		],
+	],
+];
